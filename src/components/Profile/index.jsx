30567644b0304eccdb45e2c87242ba575a1bec9f
dannycastilloo <dannycastillootiniano@gmail.com>
2023-12-23 19:14:58 -0500

    improve phone exp card

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -15,29 +15,29 @@ function Profile() {
                     <div className='icons-social'>
                         <a href="https://www.linkedin.com/in/dannycastilloo/">
                             <button className='button-contact'>
-                                <img src='./contact/Linkedin.svg' alt='Linkedin' />
+                                <img src='/contact/Linkedin.svg' alt='Linkedin' />
                             </button>
                         </a>
                         <a href="https://github.com/dannycastilloo">
                             <button className='button-contact'>
-                                <img src='./contact/Github.svg' alt='Github' />
+                                <img src='/contact/Github.svg' alt='Github' />
                             </button>
                         </a>
                         <a href="mailto:dannycastillootiniano@gmail.com">
                             <button className='button-contact'>
-                                <img src='./contact/Email.svg' alt='Email' />
+                                <img src='/contact/Email.svg' alt='Email' />
                             </button>
                         </a>
                         <a href="https://wa.link/zvm7kf">
                             <button className='button-contact'>
-                                <img src='./contact/WhatsApp.svg' alt='WhatsApp' />
+                                <img src='/contact/WhatsApp.svg' alt='WhatsApp' />
                             </button>
                         </a>
                     </div>
                 </div>
             </div>
             <div className='my-photo'>
-                <img className='photo' src='./Danny.png' alt="Danny Castillo" />
+                <img className='photo' src='/Danny.png' alt="Danny Castillo" />
             </div>
         </>
     )
