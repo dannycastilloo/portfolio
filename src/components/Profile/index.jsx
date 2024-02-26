47b86e7371f81420cb015415e81e64fe3e8b879d
dannycastilloo <dannycastillootiniano@gmail.com>
2024-02-26 02:04:11 -0500

    change colors

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -6,17 +6,16 @@ function Profile() {
             <div className='profile-content'>
                 <div className='profile-info'>
                     <div className='profile-text'>
-                        <span>Hello there! I'm a</span>
                         <h1>Web Developer & UX/UI Designer</h1>
                         <p>Passionate software developer with strong problem-solving skills and a consistent participation in projects.</p>
                     </div>
                     <div className='profile-actions'>
+                        <a href="./Danny_Castillo.pdf" target="_blank" className='cv'>Read CV</a>
                         <a href="mailto:dannycastillootiniano@gmail.com" className='hire-me'>Hire Me Now!</a>
-                        <a href="./Danny_Castillo.pdf" className='cv'>Read CV</a>
                     </div>
                 </div>
                 <div className='profile-photo'>
-                    <img src='/Danny.svg' alt="Danny Castillo" title='App Developer & Designer' />
+                    <img src='/Danny.png' alt="Danny Castillo" title='App Developer & Designer' />
                 </div>
             </div>
         </>
