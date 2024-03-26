8d01a36bf203df87b1aaa8f7d2b334e176e4a03d
dannycastilloo <dannycastillootiniano@gmail.com>
2024-03-25 21:14:25 -0500

    remove speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import App from './App.jsx'
 import './index.css'
 
