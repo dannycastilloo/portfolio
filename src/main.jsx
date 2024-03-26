75a5fe008a6cb59f9863ae23f1f79607cc41b19f
dannycastilloo <dannycastillootiniano@gmail.com>
2024-03-25 21:11:45 -0500

    add speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import App from './App.jsx'
 import './index.css'
 
